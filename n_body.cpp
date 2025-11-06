3c4cf6800ded19977441f6b17f89415ec1bc7111
Ricardo Matos <matosricardordg@gmail.com>
2025-11-06 20:43:31 -0300

    feat: add the points example of SDL3 to produce my program using this base

--- n_body.cpp
+++ n_body.cpp
@@ -1,90 +1,115 @@
-#include <iostream>
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <string>
 
-constexpr int kScreenWidth{ 640 };
-constexpr int kScreenHeight{ 480 };
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static Uint64 last_time = 0;
 
-bool init();
-bool loadMedia();
-void close();
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 
-SDL_Window* gWindow{ nullptr };
-SDL_Surface* gScreenSurface{ nullptr };
+#define NUM_POINTS 500
+#define MIN_PIXELS_PER_SECOND 30  /* move at least this many pixels per second. */
+#define MAX_PIXELS_PER_SECOND 60  /* move this many pixels per second at most. */
 
-int main(int argc, char* args[]) {
+/* (track everything as parallel arrays instead of a array of structs,
+   so we can pass the coordinates to the renderer in a single function call.) */
 
-    int exitCode { 0 };
+/* Points are plotted as a set of X and Y coordinates.
+   (0, 0) is the top left of the window, and larger numbers go down
+   and to the right. This isn't how geometry works, but this is pretty
+   standard in 2D graphics. */
+static SDL_FPoint points[NUM_POINTS];
+static float point_speeds[NUM_POINTS];
 
-    float x = 0;
-    int y = 0;
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    int i;
 
-    if(init() == false) {
-        SDL_Log("Unable to initialize program!\n");
-        exitCode = 1;
+    SDL_SetAppMetadata("Example Renderer Points", "1.0", "com.example.renderer-points");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
-    else {
-        bool quit { false };
 
-        SDL_Event e;
-        SDL_zero( e );
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/points", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-        while(quit == false) {
-            
-            while(SDL_PollEvent(&e) == true) {
-                if(e.type == SDL_EVENT_QUIT) {
-                    quit = true;
-                }
+    /* set up the data for a bunch of points. */
+    for (i = 0; i < SDL_arraysize(points); i++) {
+        points[i].x = SDL_randf() * ((float) WINDOW_WIDTH);
+        points[i].y = SDL_randf() * ((float) WINDOW_HEIGHT);
+        point_speeds[i] = MIN_PIXELS_PER_SECOND + (SDL_randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
+    }
+
+    last_time = SDL_GetTicks();
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    const Uint64 now = SDL_GetTicks();
+    const float elapsed = ((float) (now - last_time)) / 1000.0f;  /* seconds since last iteration */
+    int i;
+
+    /* let's move all our points a little for a new frame. */
+    for (i = 0; i < SDL_arraysize(points); i++) {
+        const float distance = elapsed * point_speeds[i];
+        points[i].x += distance;
+        points[i].y += distance;
+        if ((points[i].x >= WINDOW_WIDTH) || (points[i].y >= WINDOW_HEIGHT)) {
+            /* off the screen; restart it elsewhere! */
+            if (SDL_rand(2)) {
+                points[i].x = SDL_randf() * ((float) WINDOW_WIDTH);
+                points[i].y = 0.0f;
+            } else {
+                points[i].x = 0.0f;
+                points[i].y = SDL_randf() * ((float) WINDOW_HEIGHT);
             }
-            
-            x += 0.01;
-            if(x >= 1) {
-                y += 1;
-                x = 0;
-            }
-            SDL_Rect particle = { 100 + y, 100, 50, 50 };
-            Uint32 color = SDL_MapRGB(SDL_GetPixelFormatDetails(gScreenSurface->format), nullptr, 255, 0, 0);
-
-
-            SDL_FillSurfaceRect( gScreenSurface, nullptr, SDL_MapSurfaceRGB( gScreenSurface, 0xFF, 0xFF, 0xFF ) );
-            SDL_FillSurfaceRect( gScreenSurface, &particle, color );
-            
-            SDL_UpdateWindowSurface( gWindow );
+            point_speeds[i] = MIN_PIXELS_PER_SECOND + (SDL_randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
         }
     }
 
-    close();
+    last_time = now;
 
-    return exitCode;
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
+    SDL_RenderPoints(renderer, points, SDL_arraysize(points));  /* draw all the points! */
+
+    /* You can also draw single points with SDL_RenderPoint(), but it's
+       cheaper (sometimes significantly so) to do them all at once. */
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-bool init() {
-    bool success{ true };
-    
-    if(SDL_Init(SDL_INIT_VIDEO) == false) {
-        SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
-        success = false;
-    }  
-    else {
-        if( gWindow = SDL_CreateWindow( "SDL3 Tutorial: Hello SDL3", kScreenWidth, kScreenHeight, 0 ); gWindow == nullptr )
-        {
-            SDL_Log( "Window could not be created! SDL error: %s\n", SDL_GetError() );
-            success = false;
-        }
-        else
-        {
-            gScreenSurface = SDL_GetWindowSurface( gWindow );
-        }
-    }
-
-    return success;
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
 
-void close() {
-    SDL_DestroyWindow(gWindow);
-    gWindow = nullptr;
-    gScreenSurface = nullptr;
 
-    SDL_Quit();
-}
+
